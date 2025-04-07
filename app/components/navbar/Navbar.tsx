9456287fb938563ba5fa86d1ac056bf133de7d53
Silviu Stana <gamedevseals@gmail.com>
2025-04-07 14:10:49 +0300

    Initial commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
+
+const Navbar = () => {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <div
+                    className="max-w-[2520px] mx-auto
+                        xl:px-20 md:px-10 sm:px-2 px-4"
+                >
+                    <Container>
+                        <div className=" flex flex-row items-center justify-between gap-3 md:gap-0">
+                            <Logo />
+                            <Search />
+                        </div>
+                    </Container>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div
+            className="border-[1px] w-full md:w-auto py-2 rounded-full
+    shadow-sm hover:shadow-md
+    cursor-pointer transition"
+        >
+            Search
+        </div>
+    );
+};
+
+export default Search;
